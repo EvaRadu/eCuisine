852a9c1f0805e579f0ed62d5adb779bf37145be1
Juliette Sabatier <juliette.sabatier07@gmail.com>
2022-12-01 11:39:16 +0100

    toolbar with button and time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, NgModule } from '@angular/core';
-import { MatToolbarModule} from '@angular/material/toolbar';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
