f16b455d1e3b20b6767bc1b5e57241fc06a003b9
julienraph <rjulien@digitec.fr>
2022-12-04 12:37:07 +0100

    Correction merge

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { tasks } from './../tasks';
+import {Task, tasks} from './../tasks';
 
 @Component({
   selector: 'app-board',
@@ -8,5 +8,59 @@ import { tasks } from './../tasks';
 })
 
 export class BoardComponent {
-  tasks = tasks;  
+  tasks = tasks;
+  clusterize = true;
+
+  notClusterize(task: Task) {
+    if (task.pressingNumber != undefined) {
+      if(task.pressingNumber < 60 && this.clusterize) {
+        return false;
+      }
+      return true;
+    }
+    return true;
+  }
+
+  numberOfGreen() {
+    let nbGreen = 0;
+    tasks.forEach(function(task) {
+      if(task.pressingNumber != undefined) {
+        if(task.pressingNumber < 30) {
+          nbGreen += 1;
+        }
+      }
+    })
+    return nbGreen;
+  }
+
+  determineColAndRow(task: Task) {
+    if(task.pressingNumber != undefined) {
+      if(this.clusterize && task.pressingNumber < 60) {
+        return 0
+      }
+      if(task.pressingNumber < 30) {
+        return 1
+      }
+      if(task.pressingNumber < 60) {
+        return 2
+      }
+      if(task.pressingNumber < 60) {
+        return 3
+      }
+    }
+    return 1
+  }
+
+  numberOfYellow() {
+    let nbYellow = 0;
+    tasks.forEach(function(task) {
+      if(task.pressingNumber != undefined) {
+        if(task.pressingNumber >= 30 && task.pressingNumber < 60) {
+          nbYellow += 1;
+        }
+      }
+    })
+    return nbYellow;
+  }
+
 }
