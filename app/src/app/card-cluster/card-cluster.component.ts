f83b136a9c7e5b4d711dc5d4a2ae6ba40a1fb733
MiaSwery <91131345+MiaSwery@users.noreply.github.com>
2023-01-07 18:00:57 +0100

    Ajout du pin

--- app/src/app/card-cluster/card-cluster.component.ts
+++ app/src/app/card-cluster/card-cluster.component.ts
@@ -1,14 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-cluster',
   templateUrl: './card-cluster.component.html',
   styleUrls: ['./card-cluster.component.scss']
 })
-export class CardClusterComponent {
+export class CardClusterComponent implements OnInit {
   @Input() typeCluster !: string;
   @Input() numberOfCommand !: number;
   @Input() nameCluster !: string;
+  @Input() idChef !: number;
+  id !: string;
+
+  ngOnInit(): void {
+    this.id = this.idChef.toString();
+  }
 }
 
 
